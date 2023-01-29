03ab5153468231c2df99bc49b2c246a0d3c7e105
Kirill Kurbatov <kirillkurbatov482.com>
2023-01-30 00:03:15 +0300

    выполнил две задачи 7 семинара

---
+++ Example/007 №47/Program.cs
@@ -0,0 +1,24 @@
+﻿void showArray (float [,] array){
+    for(int i=0; i < array.GetLength(0); i++){
+      for(int j=0; j < array.GetLength(1); j++)
+        Console.Write($"{array[i, j]}       ");
+      Console.Write("\n");
+    }
+}
+
+
+Console.WriteLine("Введите размер массива");
+
+Console.Write("m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+float [,] array = new float [m, n];
+
+for (int i=0; i < array.GetLength(0); i++)
+  for (int j=0; j < array.GetLength(1); j++)
+    array[i, j] = Convert.ToSingle(new Random().NextDouble());
+
+showArray(array);
