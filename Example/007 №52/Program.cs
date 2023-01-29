8aa637133fb09a8bd88018d9eeb8dee8324597f8
Kirill Kurbatov <kirillkurbatov482.com>
2023-01-30 00:06:17 +0300

    выполнил all задачи 7 семинара

--- Example/007 №52/Program.cs
+++ Example/007 №52/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [,] matrix = new int[4,4];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+FillArrayMidl(matrix);
+
+
+void PrintArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+Console.Write($"{matr[i,j]} ");
+}
+Console.WriteLine();
+}
+}
+
+void FillArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+matr[i,j] = i + j;
+}
+}
+}
+
+void FillArrayMidl(int[,] matr)
+{
+Console.Write("Среднее: ");
+for (int i = 0; i < matr.GetLength(1); i++)
+{
+double sum = 0;
+for (int j = 0; j < matr.GetLength(0); j++)
+{
+sum += matr[i,j];
+}
+Console.Write($"{sum/matr.GetLength(0)}, ");
+}
+}
