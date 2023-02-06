03969d849b4eeda4fb076f8e57144f79a6140e6d
Kirill Kurbatov <kirillkurbatov482.com>
2023-02-06 20:16:08 +0300

    выполнил задания 9 семинара

---
+++ Example/009 №66/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+    int SumNum(int numFirst, int numSecond){
+
+  int sum = numFirst;
+
+  if ((numSecond - numFirst) != 0)
+    sum += SumNum(numFirst+1, numSecond);
+
+
+  return sum;
+}
+
+
+
+Console.Write("Введите первое число: ");
+int numFirst = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число (В порядке возрастания!): ");
+int numSecond = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"Сумма чисел от {numFirst} до {numSecond} равна: {SumNum(numFirst, numSecond)}");
