a748493a08dc98fc1b812bc39312636eb9e5d7a5
Kirill Kurbatov <kirillkurbatov482.com>
2023-02-06 13:40:05 +0300

    начал выполнять задачи 8 семинара

---
+++ Example/008 №60/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 60.Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+//  Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+
+Console.Write("Введите размеры массива через пробел: ");
+string[] nums = Console.ReadLine().Split("", stringSplitOptions.RemoveEmptyEntries);
+int[,,] array = GetArray(new int[] {int.Parse(nums[0]), int.Parse(nums [1]), int.Parse(nums [2]), }, 10, 99);
+PrintArray(array);
