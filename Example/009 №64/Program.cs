71aa11a5c434ac86aa7940ec8af10fcda6b44b12
Kirill Kurbatov <kirillkurbatov482.com>
2023-02-06 20:54:12 +0300

    выполнил задания 9 семинара 2,0

--- Example/009 №64/Program.cs
+++ Example/009 №64/Program.cs
@@ -1,23 +1,21 @@
-﻿int n = InputInt("Введите натуральное число ");
-int m = 1;
-if (n < 1)
-{
-    Console.WriteLine("Ввели не положительное число ");
-}
-Console.WriteLine(NaturalNumber(n, m));
+﻿void NumberToConsole(int N){
 
-int NaturalNumber(int n, int m)
-{
-    if (n == m)
-        return n;
-    else
-        Console.Write($"{NaturalNumber(n, m + 1)}, ");
-    return m;
+  if (N > 1)
+    Console.Write($"{N}, ");
+  else
+    Console.WriteLine($"{N}");
+
+  if (N > 1) 
+    NumberToConsole(N-1);
+    else if (N < 1)
+    Console.WriteLine("не натуральное число!");
+
 }
 
-int InputInt(string output)
-{
-    Console.Write(output);
-    return int.Parse(Console.ReadLine());
-}
 
+
+
+Console.Write("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+NumberToConsole(N);
